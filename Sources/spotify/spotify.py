6050b43cb63af8505319f5b9be4bd052c97d11c3
LoktionovM <m-a-x-99@yandex.by>
2022-07-06 21:02:26 +0300

    add functions

--- Sources/spotify/spotify.py
+++ Sources/spotify/spotify.py
@@ -6,17 +6,22 @@ import pandas as pd
 cid = 'dbf0dc8d604c4c609128508a05aaf09e'
 secret = 'ba6756bbc94249f58737f36628451743'
 
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+def collect_spotify_charts():
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-creator = "spotify"
-playlist_id = "37i9dQZEVXbMDoHDwVN2tF"
-data = sp.playlist_tracks(playlist_id, fields=None, limit=100, offset=0, market=None, additional_types=('track', ))
-data = data['items']
-# position = data[0]
-for i in data:
-    track_info = i['track']
-    artist = track_info['artists']
-    artist_name = artist[0]['name']
-    track = track_info['name']
-    print(f"{artist_name} - {track}")
+    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+    creator = "spotify"
+    playlist_id = "37i9dQZEVXbMDoHDwVN2tF"
+    data = sp.playlist_tracks(playlist_id, fields=None, limit=100, offset=0, market=None, additional_types=('track', ))
+    data = data['items']
+    # position = data[0]
+    num = 0
+    for each in data:
+        track_info = each['track']
+        artist = track_info['artists']
+        artist_name = artist[0]['name']
+        track = track_info['name']
+        num = num+1
+        print(f"{num}. {artist_name} - {track}")
+
+collect_spotify_charts()
--- Sources/yandex/yandex.py
+++ Sources/yandex/yandex.py
@@ -1,28 +1,29 @@
 import os
+import secrets
 from yandex_music import Client
 CHART_ID = 'belarus'
 TOKEN = os.environ.get('AQAAAABiu4_vAAgJ512cuW5RwU8HsyJ7W0gZEls')
 
+def collect_yandex_charts():
+    client = Client(TOKEN).init()
+    chart = client.chart(CHART_ID).chart
+    text = [f'🏆 {chart.title}', chart.description, '', 'Треки:']
+    for track_short in chart.tracks:
+        track, chart = track_short.track, track_short.chart
+        artists = ''
+        if track.artists:
+            artists = ' - ' + ', '.join(artist.name for artist in track.artists)
+        track_text = f'{track.title}{artists}'
+        if chart.progress == 'down':
+            track_text = '🔻 ' + track_text
+        elif chart.progress == 'up':
+            track_text = '🔺 ' + track_text
+        elif chart.progress == 'new':
+            track_text = '🆕 ' + track_text
+        elif chart.position == 1:
+            track_text = '👑 ' + track_text
+        track_text = f'{chart.position} {track_text}'
+        text.append(track_text)
+    print('\n'.join(text))
 
-client = Client(TOKEN).init()
-chart = client.chart(CHART_ID).chart
-
-
-text = [f'🏆 {chart.title}', chart.description, '', 'Треки:']
-for track_short in chart.tracks:
-    track, chart = track_short.track, track_short.chart
-    artists = ''
-    if track.artists:
-        artists = ' - ' + ', '.join(artist.name for artist in track.artists)
-    track_text = f'{track.title}{artists}'
-    if chart.progress == 'down':
-        track_text = '🔻 ' + track_text
-    elif chart.progress == 'up':
-        track_text = '🔺 ' + track_text
-    elif chart.progress == 'new':
-        track_text = '🆕 ' + track_text
-    elif chart.position == 1:
-        track_text = '👑 ' + track_text
-    track_text = f'{chart.position} {track_text}'
-    text.append(track_text)
-print('\n'.join(text))
+collect_yandex_charts()
