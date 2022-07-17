8086e038eeb56ed8cdb9f9558c8aa6b5b7fca89b
LoktionovM <m-a-x-99@yandex.by>
2022-07-17 11:34:13 +0300

    add perfect database structure

--- Project/eraser.py
+++ Project/eraser.py
@@ -1,12 +1,16 @@
 import pymongo
 mongo = pymongo.MongoClient()
-mydb = mongo["charts"]
-spotify = mydb["spotify"]
-yandex = mydb["yandex"]
-def erase_all_collections(col1, col2):
-    col1.drop({})
-    col2.drop({})
+songs_db = mongo["charts"]
+
+spotify = songs_db["spotify"]
+
+yandex = songs_db["yandex"]
 
 
-erase_all_collections(spotify,yandex)
+def erase_all_collections(song_spotify, song_yandex,):
+    song_spotify.drop({})
+    song_yandex.drop({})
+
+
+erase_all_collections(spotify, yandex)
 ##ЗАСУНУТЬ ПЕРЕД ВОРКЕРАМИ ВСЁ В ОДНУ ФУНКЦИЮ
